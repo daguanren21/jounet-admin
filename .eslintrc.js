3adfc1bddaa2c3f6fe537742b93067d68b32ed1d
daguanren21 <1363917353@qq.com>
2023-03-27 14:22:51 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": ["@daguanren21/eslint-config"],
+    "overrides": [
+    ],
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue 3 + TypeScript + Vite
+## 搭建基础框架
+```javascript
+pnpm create vite vue-app --template vue-ts
+```
+## Eslint 配置(TD)
+```javascript
+```
